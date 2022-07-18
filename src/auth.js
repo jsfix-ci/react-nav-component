fe5762228deea54e6a8b2eba352aa5b24afb5bad
jsfix.live analysis <info@coana.tech>
2022-07-18 11:39:22 +0200

    JSFIX patches

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,5 @@
 import auth0 from 'auth0-js';
-import createHistory from 'history/createBrowserHistory';
+import { createBrowserHistory as createHistory } from 'history';
 
 let history = createHistory({
   forceRefresh: true
